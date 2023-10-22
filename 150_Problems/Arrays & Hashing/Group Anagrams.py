4255b73e70932e29387a3810e192165c9ce4be23
Shashank Singh <shashanksingh2409@outlook.com>
2023-10-22 15:07:34 -0500

    problem 4/150

---
+++ 150_Problems/Arrays & Hashing/Group Anagrams.py
@@ -0,0 +1,21 @@
+# Medium
+# https://leetcode.com/problems/group-anagrams/description/
+
+#############
+# Solution: 1
+#############
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        str_dict = {}
+
+        for i in strs:
+            sorted_str = ''.join(sorted(i))
+            if sorted_str in str_dict:
+                str_dict[sorted_str].append(i)
+            else:
+                str_dict[sorted_str] = [i]
+        
+        return list(str_dict.values())
+
+#Runtime 92 ms Beats 85.33%
+#Memory 19.5 MB Beats 89.8%
